555efb6764f6f58f585657581000d10922fdaa7e
msiyem <siyem2574@gmail.com>
2026-02-09 20:46:48 +0600

    fix cross origin issue

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,10 +1,10 @@
 import cors from "cors";
 
 const allowedOrigins = [
-  "http://localhost:5173",   // Vite
-  "http://localhost:3000",   // React
-  "http://localhost:4173",   // Preview
-  "https://pos-system-six-jade.vercel.app/",   // Preview
+  "http://localhost:5173", // Vite
+  "http://localhost:3000", // React
+  "http://localhost:4173", // Preview
+  "https://pos-system-six-jade.vercel.app", // Preview
 ];
 
 export const corsConfig = cors({
@@ -12,12 +12,16 @@ export const corsConfig = cors({
     // allow server-to-server or Postman
     if (!origin) return callback(null, true);
 
-    if (allowedOrigins.includes(origin)) {
+    const normalizedOrigin = origin.endsWith("/")
+      ? origin.slice(0, -1)
+      : origin;
+
+    if (allowedOrigins.includes(normalizedOrigin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true, // allow cookies
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 });
